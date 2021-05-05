fde1933f0259ec3743dd03f249b0302f4f7ab87a
Auyy <71812937+Auyy@users.noreply.github.com>
2021-05-05 15:39:04 +0700

    MVC Sqlite

---
+++ TrainingDotNetCoreMVC/TrainingDotNetCoreMVC/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TrainingDotNetCoreMVC.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Nmae { get; set; }
+
+        [DataType(DataType.ImageUrl)]
+        public string img { get; set; }
+        public string show { get; set; }
+        public string type { get; set; }
+        public string time { get; set; }
+    }
+}
